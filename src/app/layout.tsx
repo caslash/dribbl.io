f7a525bb5990f9b7d8d710b0f0de538c76355360
caslash <caslash@TCHALLA.net.sep.com>
2025-01-29 14:42:31 -0500

    Added fonts and added getting player images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,15 @@ import '@/styles/globals.css';
 import { Providers } from '@/app/providers';
 import NBANavbar from '@/components/navbar';
 import { Toaster } from 'react-hot-toast';
+import { sfFont } from '@/styles/sfFont';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className={`${sfFont.variable}`} suppressHydrationWarning>
       <body>
         <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
           <NBANavbar />
-          <Toaster position="top-center" />
+          <Toaster position="bottom-center" />
           <main>{children}</main>
         </Providers>
       </body>
