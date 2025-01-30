823949b878dfebcf6e9616a9114e616dbdb57627
caslash <caslash@TCHALLA.net.sep.com>
2025-01-30 10:55:29 -0500

    Started separating logic from views

--- src/app/singleplayer/page.tsx
+++ src/app/singleplayer/page.tsx
@@ -1,90 +1,49 @@
 'use client';
 
+import { CareerPathView } from '@/components/careerpathview';
 import CorrectAnswerView from '@/components/correctanswerview';
 import PlayerSearchBar from '@/components/playersearchbar';
-import TeamLogo from '@/components/teamlogo';
+import useCareerPath from '@/hooks/useCareerPath';
+import useConfetti from '@/hooks/useConfetti';
 import { Button } from '@heroui/react';
 import { Player } from '@prisma/client';
-import confetti from 'canvas-confetti';
 import { useTheme } from 'next-themes';
-import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { toast } from 'react-toastify';
-import { v4 } from 'uuid';
-import { getRandomPlayer } from '../actions';
 
 export default function SinglePlayer() {
   const { theme } = useTheme();
-  const [player, setPlayer] = useState<Player | null>();
-  const [streak, setStreak] = useState<number>(0);
+  const { player, onStart, checkGuess, streak, setPlayerPoolFilter } = useCareerPath();
+  const { onConfetti } = useConfetti();
 
-  const fetchRandomPlayer = () => {
-    getRandomPlayer({ total_games_played: { gte: 800 } }).then(setPlayer);
+  useEffect(
+    () => setPlayerPoolFilter({ isActive: { equals: true }, total_games_played: { gte: 800 } }),
+    [setPlayerPoolFilter],
+  );
+
+  const correctAction = (correctPlayer: Player) => {
+    toast(<CorrectAnswerView isCorrect correctPlayer={correctPlayer} />, { theme });
+    onConfetti();
   };
 
-  const onConfetti = () => {
-    const end = Date.now() + 1 * 1000;
-    const colors = ['#1d428a', '#c8102e', '#ffffff'];
-
-    const frame = () => {
-      if (Date.now() > end) return;
-
-      confetti({
-        particleCount: 2,
-        angle: 60,
-        spread: 55,
-        startVelocity: 60,
-        origin: { x: 0, y: 0.5 },
-        colors,
-      });
-      confetti({
-        particleCount: 2,
-        angle: 120,
-        spread: 55,
-        startVelocity: 60,
-        origin: { x: 1, y: 0.5 },
-        colors,
-      });
-
-      requestAnimationFrame(frame);
-    };
-
-    frame();
-  };
-
-  const checkGuessedPlayer = (key: React.Key | null) => {
-    if (key) {
-      const previousPlayer = player!;
-      if (key == player?.id) {
-        toast(<CorrectAnswerView isCorrect correctPlayer={previousPlayer} />, { theme });
-        onConfetti();
-        setStreak(streak + 1);
-      } else {
-        toast(<CorrectAnswerView isCorrect={false} correctPlayer={previousPlayer} />, {
-          theme,
-        });
-        setStreak(0);
-      }
-      fetchRandomPlayer();
-    }
+  const incorrectAction = (correctPlayer: Player) => {
+    toast(<CorrectAnswerView isCorrect={false} correctPlayer={correctPlayer} />, { theme });
   };
 
   return (
     <div className="flex flex-col h-full items-center m-16 space-y-8">
-      <Button onPressEnd={() => fetchRandomPlayer()}>Start Game</Button>
+      <Button onPress={onStart}>Start Game</Button>
       {player && (
         <div className="flex flex-col items-center">
           <p className={`font-black text-xl`}>Streak:</p>
           <p className={`font-semibold text-6xl`}>{streak}</p>
         </div>
       )}
-      {player?.team_history && (
-        <div className="flex flex-row">
-          {player.team_history.split(',').map((id: string) => (
-            <TeamLogo key={v4()} teamId={id} />
-          ))}
-        </div>
-      )}
-      <PlayerSearchBar className="w-1/2" onSelectionChange={(key) => checkGuessedPlayer(key)} />
+      <CareerPathView player={player} />
+      <PlayerSearchBar
+        className="w-1/2"
+        onSelectionChange={(key) => checkGuess(key, correctAction, incorrectAction)}
+      />
     </div>
   );
 }
