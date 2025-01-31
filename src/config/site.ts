304c859da2b310996800b7bd8d5ecf66fd90c879
caslash <caslash@TCHALLA.net.sep.com>
2025-01-31 10:10:39 -0500

    Remove logic from player table

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,11 +1,20 @@
 export type SiteConfig = typeof siteConfig;
 
-export const siteConfig = {
+type NavItem = {
+  label: string;
+  href: string;
+};
+
+export const siteConfig: { name: string; navItems: NavItem[] } = {
   name: 'NBA Career Game',
   navItems: [
     {
       label: 'Home',
       href: '/',
+    },
+    {
+      label: 'Players',
+      href: '/players',
     },
     {
       label: 'Multiplayer',
