631ef3c84913301f529f955b4d4c67355fb17b83
Cameron Slash <calexslash@aol.com>
2025-02-02 03:09:18 -0500

    Added slowly unveiling logos

--- src/components/careerpath/testcareerpathview.tsx
+++ src/components/careerpath/testcareerpathview.tsx
@@ -1,30 +1,42 @@
 'use client';
 
-import { Player } from '@prisma/client';
+import useUnveilLogos from '@/hooks/useUnveilLogos';
+import { useEffect } from 'react';
 import { v4 } from 'uuid';
 import TeamLogo from '../teamlogo';
 
 export function TestCareerPathView({
-  player,
+  teamHistory,
   theme,
-}: Readonly<{ player: Player; theme: string | undefined }>) {
+}: Readonly<{
+  teamHistory: string[];
+  theme: string | undefined;
+}>) {
+  const { visibleIndexes, unveilRandomLogoIndex } = useUnveilLogos(teamHistory);
+
+  useEffect(() => {
+    let unveilInterval = setInterval(() => {
+      unveilRandomLogoIndex(unveilInterval);
+    }, 5000);
+
+    return () => {
+      clearInterval(unveilInterval);
+    };
+  }, []);
+
   return (
     <div>
-      {player.team_history && (
-        <div className="flex flex-row">
-          {player.team_history.split(',').map((id: string, index: number) => (
-            <div
-              key={v4()}
-              className="grid grid-rows-1 aspect-square shadow-lg p-4  mx-4 rounded-xl bg-neutral-200 dark:bg-neutral-800"
-            >
-              <svg height={100} width={100} xmlns="http://www.w3.org/2000/svg">
-                <rect height={100} width={100} fillOpacity={0} />
-              </svg>
-              <TeamLogo isHidden={false} teamId={id} theme={theme} />
-            </div>
-          ))}
-        </div>
-      )}
+      <div className="flex flex-row">
+        {teamHistory.map((id: string, index: number) => (
+          <TeamLogo
+            key={v4()}
+            className=" max-h-[100] p-1 mx-2 rounded-xl shadow-xl bg-neutral-200 dark:bg-neutral-800"
+            isHidden={!visibleIndexes.includes(index)}
+            teamId={id}
+            theme={theme}
+          />
+        ))}
+      </div>
     </div>
   );
 }
