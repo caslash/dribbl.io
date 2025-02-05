f286b9f5d77867e5c868ff48546417fa2a8ad17b
Cameron Slash <calexslash@aol.com>
2025-02-05 15:00:52 -0500

    Add home page buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import { Button } from '@heroui/react';
+import NextLink from 'next/link';
+
 export default function Home() {
-  return <h1>Hello, World</h1>;
+  return (
+    <div className="h-screen w-screen grid grid-cols-3 grid-rows-3">
+      <div className="col-start-2 row-start-2 place-self-center flex flex-row space-x-8">
+        <Button as={NextLink} href="/singleplayer" variant="shadow" color="primary" radius="sm">
+          Single Player
+        </Button>
+        <Button as={NextLink} href="/multiplayer" variant="shadow" color="secondary" radius="sm">
+          Multiplayer
+        </Button>
+      </div>
+    </div>
+  );
 }
