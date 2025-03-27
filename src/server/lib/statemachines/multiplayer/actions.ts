a8005d59e84aab8564bb7b79681b5ef0599e54dd
Cameron Slash <calexslash@aol.com>
2025-03-27 16:47:38 -0400

    Start adding timer

--- src/server/lib/statemachines/multiplayer/actions.ts
+++ src/server/lib/statemachines/multiplayer/actions.ts
@@ -1,18 +1,13 @@
-import { Room } from '@/server/lib/models/room';
-import { UserGameInfo } from '@/server/lib/statemachines/multiplayer/gamemachine';
-import { Player } from '@prisma/client';
-import { Server } from 'socket.io';
+import { MultiplayerGuess } from '@/server/lib/models/gamemachine';
+import {
+  MultiplayerContext,
+  UserGameInfo,
+} from '@/server/lib/statemachines/multiplayer/gamemachine';
+import { AnyEventObject, assign } from 'xstate';
 
 type ActionProps = {
-  context: {
-    io: Server;
-    room: Room;
-    gameState: {
-      users: UserGameInfo[];
-      currentPlayer: Player | undefined;
-      validAnswers: Player[];
-    };
-  };
+  context: MultiplayerContext;
+  event: AnyEventObject;
 };
 
 export const sendPlayerToRoom = ({ context }: ActionProps) => {
@@ -26,4 +21,41 @@ export const sendPlayerToRoom = ({ context }: ActionProps) => {
   } catch (err) {
     throw Error(`Socket could not be found: ${err}`);
   }
+};\
+
+const updateUserScore = (users: UserGameInfo[], currentGuess: MultiplayerGuess): UserGameInfo[] => {
+  const otherUsers = users.filter((user) => user.info.id != currentGuess.userId);
+  const currentUser = users.find((user) => user.info.id == currentGuess.userId)!;
+
+  return [...otherUsers, { ...currentUser, score: currentUser.score + 1 }];
+};
+
+export const checkGuess = ({ context, event }: ActionProps) => {
+  const isCorrect = !!context.gameState.validAnswers.find(
+    (player) => player.id === event.guess.guessId,
+  );
+  if (isCorrect) {
+    assign({
+      ...context,
+      gameState: {
+        ...context.gameState,
+        users: updateUserScore(context.gameState.users, event.guess),
+      },
+    });
+  }
+};
+
+export const decrementTimer = ({ context }: ActionProps) => {
+  const { io, room, gameState } = context;
+  const timeLeft = gameState.timeLeft - 1;
+
+  assign({
+    ...context,
+    gameState: {
+      ...context.gameState,
+      timeLeft,
+    },
+  });
+
+  io?.to(room.id).emit('timer_updated', { timeLeft });
 };
