577e64ef577e8a3d59b42a8ddf2721132dce3c58
Cameron Slash <calexslash@aol.com>
2025-05-27 23:56:48 -0400

    Refactor playersearchbar to use /api/players endpoint

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
         sf: ['var(--font-sf)'],
       },
       zIndex: {
-        60: "60",
+        60: '60',
       },
       borderRadius: {
         lg: 'var(--radius)',
--- packages/types/src/statemachine/actors.ts
+++ packages/types/src/statemachine/actors.ts
@@ -29,6 +29,6 @@ export const generateRound = fromPromise(async ({ input }: RoundInput): Promise<
   // };
   return {
     validAnswers: [],
-    players: []
-  }
+    players: [],
+  };
 });
