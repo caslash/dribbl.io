94fe1e859ccdcd12aedaefdd324965310f8a85da
Cameron Slash <calexslash@aol.com>
2025-09-01 19:53:05 -0400

    Use env vars for S3Client

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -2,10 +2,12 @@ import { AvatarService } from '@/users/avatar.service';
 import { S3Service } from '@/users/s3.service';
 import { S3Client } from '@aws-sdk/client-s3';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [UsersController],
   providers: [
     UsersService,
@@ -13,7 +15,17 @@ import { UsersService } from './users.service';
     AvatarService,
     {
       provide: S3Client,
-      useValue: new S3Client(),
+      useFactory: (configService: ConfigService) => {
+        return new S3Client({
+          endpoint: configService.get<string>('AWS_S3_ENDPOINT'),
+          forcePathStyle: true,
+          credentials: {
+            accessKeyId: configService.get<string>('AWS_ACCESS_KEY_ID') ?? '',
+            secretAccessKey: configService.get<string>('AWS_SECRET_ACCESS_KEY') ?? '',
+          },
+        });
+      },
+      inject: [ConfigService],
     },
   ],
   exports: [UsersService],
